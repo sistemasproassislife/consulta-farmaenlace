2d79f005787a8ff6bcdbb14fde79b6ecf4dd1a0e
sistemasproassislife <sistemas.proassismed@gmail.com>
2026-04-15 16:40:51 -0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Consulta Farmaenlace",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
