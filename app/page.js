a8a246245e9520f1a533318d7eb641d4de099fad
sistemasproassislife <sistemas.proassismed@gmail.com>
2026-04-15 17:23:03 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,36 @@
-// update
+"use client";
+import { useState } from "react";
+
 export default function Home() {
+  const [cedula, setCedula] = useState("");
+  const [data, setData] = useState(null);
+
+  const consultar = async () => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_APEX_API_URL}?nrodoc=${cedula}`
+      );
+      const json = await res.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div style={{ padding: 20 }}>
-      <h1>Funciona 🚀</h1>
+      <h2>Consulta de cobertura</h2>
+
+      <input
+        type="text"
+        placeholder="Ingrese cédula"
+        value={cedula}
+        onChange={(e) => setCedula(e.target.value)}
+      />
+
+      <button onClick={consultar}>Consultar</button>
+
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
